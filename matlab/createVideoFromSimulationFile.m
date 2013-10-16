958ca36231897751e2b25cf558dddedc95ab582b
Dominik <schdomin@gmail.com>
2013-10-16 14:17:31 +0200

    updated

--- matlab/createVideoFromSimulationFile.m
+++ matlab/createVideoFromSimulationFile.m
@@ -1,5 +1,8 @@
 function createVideoFromSimulationFile( )
 
+%ds maximum absolute velocity (empiric value)
+dAbsoluteVelocity = 5.0;
+
 %ds open the file
 fileID = fopen( '../bin/simulation.txt' );
 
@@ -14,7 +17,7 @@ T = cCell{2};
 disp( ['N: ', num2str( N ) ] );
 disp( ['T: ', num2str( T ) ] );
 
-%ds get the next lines for the initial frame
+%ds get the next lines for the initial frame (read first timestep)
 cCell = textscan( fileID, '%f %f %f %f %f %f', N );
 
 %ds prepare video writer
@@ -24,8 +27,17 @@ writerObj = VideoWriter('simulation.avi');
 writerObj.FrameRate = 500;
 open( writerObj );
 
+%ds get absolute velocity
+vecAbsoluteVelocity = sqrt( cCell{4}.^2 + cCell{5}.^2 + cCell{6}.^2 );
+
+%ds scale it
+vecAbsoluteVelocity = vecAbsoluteVelocity./dAbsoluteVelocity;
+
+%ds color vector
+vecColor = [ vecAbsoluteVelocity, zeros( N, 1 ), 1-vecAbsoluteVelocity ];
+
 %ds generate initial data
-scatter3( cCell{1}, cCell{2}, cCell{3}, 'fill', 'blue' );
+scatter3( cCell{1}, cCell{2}, cCell{3}, 25, vecColor, 'fill', 'MarkerEdgeColor', 'black' );
 axis tight;
 set( gca, 'nextplot' ,'replacechildren' );
 set( gcf, 'Renderer' ,'zbuffer' );
@@ -44,8 +56,17 @@ for u = 2:1:T
     V = cCell{5};
     W = cCell{6};
     
+    %ds get absolute velocity
+    vecAbsoluteVelocity = sqrt( U.^2 + V.^2 + W.^2 );
+
+    %ds scale it
+    vecAbsoluteVelocity = vecAbsoluteVelocity./dAbsoluteVelocity;
+
+    %ds color vector
+    vecColor = [ vecAbsoluteVelocity, zeros( N, 1 ), 1-vecAbsoluteVelocity ];
+    
     %ds create a figure
-    scatter3( X, Y, Z, 'fill', 'blue' );
+    scatter3( X, Y, Z, 25, vecColor, 'fill', 'MarkerEdgeColor', 'black' );
     frame = getframe;
     writeVideo( writerObj, frame );
     
